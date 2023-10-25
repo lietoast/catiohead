e74136c7ab82ddb1b9b56b407904c00e091e7213
lietoast <hms_endeavour_dj@hotmail.com>
2023-10-25 09:40:37 +0800

    Implemented initialization procedure of master service(Not tested)

--- master/chunck_services.go
+++ master/chunck_services.go
@@ -1,8 +1,13 @@
 package master
 
 import (
+	"context"
 	"fmt"
 	"sync"
+	"time"
+
+	"github.com/lietoast/catiohead/pb"
+	"google.golang.org/grpc"
 )
 
 // possible status of a chunck service
@@ -10,6 +15,7 @@ const (
 	DEAD    = 0
 	HEALTHY = 1
 	UNKNOWN = 2
+	DYING   = 3
 )
 
 // config file path
@@ -17,11 +23,15 @@ const manifestPath = "./manifest" // test environment
 
 // info of one chunck service
 type chunckService struct {
-	id       int    // unmodifiable service ID
-	address  string // IP address of this service
-	port     int
-	status   int
-	diskFree uint64 // free disk space remaining in this server
+	id              int    // unmodifiable service ID
+	address         string // IP address of this service
+	port            int
+	status          int
+	diskFree        uint64           // free disk space remaining in this server
+	recentChunckNum int              // chunck created in 60 seconds
+	serviceInfoMtx  *sync.RWMutex    // lock for service information
+	conn            *grpc.ClientConn // grpc connection to this service
+	connMtx         *sync.Mutex      // lock for grpc connection
 }
 
 // service ID -> service info
@@ -54,7 +64,25 @@ func withChunckServiceStatus(status int) chunckServiceOption {
 	}
 }
 
-func NewChunckService(options ...chunckServiceOption) *chunckService {
+func withChunckServiceInfoLock() chunckServiceOption {
+	return func(cs *chunckService) {
+		cs.serviceInfoMtx = new(sync.RWMutex)
+	}
+}
+
+func withChunckServiceConnLock() chunckServiceOption {
+	return func(cs *chunckService) {
+		cs.connMtx = new(sync.Mutex)
+	}
+}
+
+func withRecentChunckNum(recentChunckNum int) chunckServiceOption {
+	return func(cs *chunckService) {
+		cs.recentChunckNum = recentChunckNum
+	}
+}
+
+func createChunckService(options ...chunckServiceOption) *chunckService {
 	service := new(chunckService)
 	for _, f := range options {
 		f(service)
@@ -76,11 +104,14 @@ func initServicesInfo() {
 					continue
 				}
 
-				chunckServices[id] = NewChunckService(
+				chunckServices[id] = createChunckService(
 					withChunckServiceID(id),
 					withChunckServiceAddress(ip),
 					withChunckServicePort(port),
 					withChunckServiceStatus(UNKNOWN),
+					withRecentChunckNum(0),
+					withChunckServiceInfoLock(),
+					withChunckServiceConnLock(),
 				)
 			}
 		})
@@ -89,5 +120,69 @@ func initServicesInfo() {
 func init() {
 	// init services
 	initServicesInfo()
-	// TODO: check status of services
+	// check status of services
+	checkChunckServicesStatus()
+}
+
+func checkChunckServicesStatus() {
+	for i := 0; i < len(chunckServices); i++ {
+		checkChunckServiceStatus(i)
+	}
+}
+
+func checkChunckServiceStatus(id int) {
+	if service, ok := chunckServices[id]; !ok {
+		return
+	} else {
+		if service.conn == nil {
+			service.connMtx.Lock()
+			if service.conn == nil {
+				conn, err := pb.InsecureConnect(service.address, service.port)
+				if err != nil {
+					service.conn = nil
+					service.setStatus(DEAD)
+					service.connMtx.Unlock()
+					return
+				}
+				service.conn = conn
+			}
+			service.connMtx.Unlock()
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+
+		client := pb.NewHealthCheckServiceClient(service.conn)
+
+		resp, err := client.CheckHealthStatus(ctx, &pb.Ping{})
+		if err != nil {
+			service.connMtx.Lock()
+			service.conn = nil
+			service.connMtx.Unlock()
+			service.setStatus(DEAD)
+			return
+		}
+
+		service.setDiskFree(resp.DiskFree)
+		service.setStatus(int(resp.Status))
+		service.setRecentChunckNum(int(resp.RecentChunkNum))
+	}
+}
+
+func (c *chunckService) setStatus(status int) {
+	c.serviceInfoMtx.Lock()
+	c.status = status
+	c.serviceInfoMtx.Unlock()
+}
+
+func (c *chunckService) setDiskFree(diskFree uint64) {
+	c.serviceInfoMtx.Lock()
+	c.diskFree = diskFree
+	c.serviceInfoMtx.Unlock()
+}
+
+func (c *chunckService) setRecentChunckNum(recentChunckNum int) {
+	c.serviceInfoMtx.Lock()
+	c.recentChunckNum = recentChunckNum
+	c.serviceInfoMtx.Unlock()
 }
---
+++ pb/client.go
@@ -0,0 +1,17 @@
+package pb
+
+import (
+	"fmt"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func InsecureConnect(address string, port int) (*grpc.ClientConn, error) {
+	address = fmt.Sprintf("%s:%d", address, port)
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
